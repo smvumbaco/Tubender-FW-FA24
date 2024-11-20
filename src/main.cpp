f02b71a81af37ed1275286d7fa45a560d64948c1
SeanSuleski <seansuleski@gmail.com>
2024-11-19 20:26:48 -0500

    matt main

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,22 @@
-#include <Arduino.h>
 #include "TubenderStateMachine.hpp"
+#include <tft.hpp>
+#include <Arduino.h>
+#include <Adafruit_RA8875.h>
 
-TubenderStateMachine stateMachine();
+#define RA8875_CS  5   // Chip select pin
+#define RA8875_RST 4   // Reset pin
+
+Adafruit_RA8875 adafruitTFT = Adafruit_RA8875(RA8875_CS, RA8875_RST);
 
 void setup() {
+    Serial.begin(115200);
+    TFT myTFT(adafruitTFT);
+    myTFT.initialize();
+    myTFT.displayStartMenu();
 
-    Serial.begin(9600);
-
-    // TubenderStateMachine.initializePins();
 
 }
 
 void loop() {
-    // TubenderStateMachine.handleState();
+
 }
